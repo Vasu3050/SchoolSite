6df759e2cf8b851ef7a8de1cfbb5fec38214f477
Vasu3050 <23u0707@students.git.edu>
2025-08-19 23:26:14 +0530

    Add image upload functionality to NoticeBoard model and implement Cloudinary integration

--- src/Models/noticBoard.models.js
+++ src/Models/noticBoard.models.js
@@ -1,37 +1,40 @@
 import mongoose from "mongoose";
 
 const noticeBoardSchema = new mongoose.Schema(
-    {
-        title : {
-            type : String,
-            required : true,
-            trim: true,
-            minlength: 3,
-            maxlength: 100,
-        },
-        description : {
-            type : String,
-            required : true,
-            trim: true,
-            minlength: 10,
-            maxlength: 500,
-        },
-        postedBy : {
-            type : mongoose.Types.ObjectId,
-            ref : 'User',
-            required : true,
-        },
-        expiryDate : {
-            type : Date,
-            required : true,
-        },
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 100,
     },
-    {
-        timestamps: true,
-    }
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 500,
+    },
+    imageUrl: {
+      type: String,
+      required: false,
+    }, // cloudinary URL
+    postedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    expiryDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 noticeBoardSchema.index({ expiryDate: 1 }, { expireAfterSeconds: 0 }); // time to leave feature
-
 
 export const NoticeBoard = mongoose.model("NoticeBoard", noticeBoardSchema);
